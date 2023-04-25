3a13cc8c9de4d8fd45c563b8c267a2266defefcf
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-04-25 11:02:30 +0800

    admins changed

--- helpers/sendToAdmins.js
+++ helpers/sendToAdmins.js
@@ -1,12 +1,10 @@
 const {getUserFormDB} = require("./getUserFormDB");
-const sendToAdmins = (message, bot) => {
-    const admins = ['badavoo', 'ivan_tyumenyev']
-    admins.forEach(async (admin) => {
-        const {user, error} = await getUserFormDB(admin);
-        if(user.chat_id) {
-            await bot.telegram.sendMessage(user.chat_id, message);
-        }
-    });
+const sendToAdmins = async (message, bot) => {
+    // const admins = ['badavoo', 'ivan_tyumenyev']
+    const {user, error} = await getUserFormDB('badavoo');
+    if(user.chat_id) {
+        await bot.telegram.sendMessage(user.chat_id, message);
+    }
 }
 
 module.exports = {sendToAdmins};
