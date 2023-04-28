08f0dc05c1ad8ad6c8619000654929660b195910
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-04-28 16:10:08 +0800

    ad

--- api/daily.js
+++ api/daily.js
@@ -3,6 +3,16 @@ const {Telegraf} = require("telegraf");
 const prodToken = '5888882359:AAGcta__XatJMomOeSNIzTvQ9k5y7ejP8jQ'
 const bot = new Telegraf(prodToken);
 
+module.exports = async (req, res) => {
+    try {
+        // Your request handling logic here
+        res.status(200).send('Cron job executed successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
 const dailyFuncs = async () => {
     await bot.telegram.sendMessage(208165379, 'cron')
     console.log('cron works')
@@ -10,3 +20,5 @@ const dailyFuncs = async () => {
 
 console.log('cron works outside')
 dailyFuncs()
+
+
